7f6947f385112f20d7c69d952331e637417928ca
WinsMainPC <winston.bocol126@gmail.com>
2025-07-18 23:40:37 +0800

    updated entity spawning from old one

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -32,9 +32,10 @@ def generate_launch_description():
              )
 
     # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='spawn_entity.py',
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-name', 'my_bot',
+                                   '-z', '0.1'],
                         output='screen')
 
 
